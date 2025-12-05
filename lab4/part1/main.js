2d908c309b2a4b61d1543bf080c60d67a58833d3
Amshika <amshika.gautam@dcmail.ca>
2025-12-04 19:28:55 -0500

    completed

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -65,6 +65,6 @@ function generateStory() {
   }
 
   // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
