dedce17582ecbbf1b489ca5becde42e6c3a3f6c2
Amshika <amshika.gautam@dcmail.ca>
2025-12-05 10:32:10 -0500

    animating the ball using functions

--- lab4/part4/script.js
+++ lab4/part4/script.js
@@ -51,7 +51,6 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
-
 }
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
 testBall.x;
@@ -59,4 +58,33 @@ testBall.size;
 testBall.color;
 testBall.draw();
 
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+loop();
 
