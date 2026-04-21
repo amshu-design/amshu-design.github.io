5c8b25cfd14af505971411ebea0ceb737454347e
Amshika <amshika.gautam@dcmail.ca>
2026-04-21 16:54:01 -0400

    Added gitignore and cleaned project

---
+++ PrivateVaultApp/PrivateVaultApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PrivateVaultApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+        string masterPassword = "1234";
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            if (passwordEntry.Text == masterPassword)
+            {
+                messageLabel.TextColor = Colors.Green;
+                messageLabel.Text = "Access Granted ✅";
+            }
+            else
+            {
+                messageLabel.TextColor = Colors.Red;
+                messageLabel.Text = "Wrong Password ❌";
+            }
+        }
+
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ PrivateVaultApp/PrivateVaultApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrivateVaultApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
